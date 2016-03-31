3cc6e86a4a71dc7b20016dec4993da3a35481540
icesmoo00th <jasonrahm@gmail.com>
2016-03-30 19:54:38 -0600

    Initial commit for Course 3: Javscript Basics. Simple story maker with prompts and variable concatination

---
+++ course3-javascript-basics/scripts.js
@@ -0,0 +1,5 @@
+var adjective = prompt("Please type an adjective");
+var verb = prompt("Please type a verb");
+var noun = prompt ("Please type a noun");
+var sentence = "<h2>There once was an " + adjective + " programmer who wanted to use JavaScript to " + verb + " the " + noun + ".</h2>";
+document.write(sentence);
