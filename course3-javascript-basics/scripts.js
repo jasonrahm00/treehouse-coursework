e232ed3d4554f0ccf2c5f50655ee3e18a8bd265f
icesmoo00th <jasonrahm@gmail.com>
2016-03-31 18:00:00 -0600

    added more conditional statements to allow for more guesses

--- course3-javascript-basics/scripts.js
+++ course3-javascript-basics/scripts.js
@@ -1,7 +1,21 @@
+var correctGuess = false;
 var randomNumber = Math.floor(Math.random() * 6) + 1;
-var guess = prompt("I am thinking of a number between 1 and 6. What is it?");
-if (parseInt(guess) === randomNumber) {
-  alert("My guessed right");
+var guess = parseInt(prompt("Ia m thinking of a number between 1 and 6. What is it?"));
+if (guess === randomNumber) {
+  correctGuess = true;
+} else if (guess < randomNumber) {
+  var guessMore = prompt("Try again. The number I am thinking of is more than " + guess);
+  if (parseInt(guessMore) === randomNumber) {
+    correctGuess = true;
+  }
+} else if (guess > randomNumber) {
+  var guessMore = prompt("Try again. The number I am thinking of is less than " + guess);
+  if (parseInt(guessMore) === randomNumber) {
+    correctGuess = true;
+  }
+}
+if (correctGuess) {
+  document.write("<p>You guessed the number!</p>");
 } else {
-  alert("My number was " + randomNumber + ". You chose " + guess + ". You guessed wrong.");
+  document.write("<p>Sorry. The number was " + randomNumber + ".</p>");
 }
