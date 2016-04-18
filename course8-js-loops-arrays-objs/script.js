553b886fa3b9e53996ee7dfc22185e2d30d904ad
icesmoo00th <jasonrahm@gmail.com>
2016-04-18 15:31:14 -0600

    created simple program to generate random number and having computer try to guess that random number

--- course8-js-loops-arrays-objs/script.js
+++ course8-js-loops-arrays-objs/script.js
@@ -1,10 +1,16 @@
-function randomNumber(upper) {
+var upper = 10000;
+var randomNumber = getRandomNumber(upper);
+var guess;
+var attempts = 0;
+
+function getRandomNumber(upper) {
   return Math.floor( Math.random() * upper ) + 1;
 }
 
-var counter = 0;
-while (counter < 10) {
-  var randNum = randomNumber(6)
-  document.write(randNum + ' ');
-  counter += 1;
+while ( guess !== randomNumber ) {
+  guess = getRandomNumber(upper);
+  attempts += 1;
 }
+
+document.write("<p>The random number was: " + randomNumber + "</p>");
+document.write("<p>It took the computer " + attempts + " attempts to get it right.</p>")
