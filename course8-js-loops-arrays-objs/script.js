e3c3978b8428272f25a95e0edae2561fbae6aa87
icesmoo00th <jasonrahm@gmail.com>
2016-04-18 16:01:13 -0600

    use for loop to automatically generate divs on the page

--- course8-js-loops-arrays-objs/script.js
+++ course8-js-loops-arrays-objs/script.js
@@ -1,19 +1,6 @@
-var randomNumber = getRandomNumber(10);
-var guess;
-var guessCount = 0;
-var correctGuess = false;
+var html = '';
 
-function getRandomNumber(upper) {
-  return Math.floor( Math.random() * upper ) + 1;
+for (var i = 0; i <= 10; i += 1) {
+  html += '<div>' + i + '</div>'
 }
-
-do {
-  guess = prompt('I am thinking of a number between 1 and 10. What is it?');
-  guessCount += 1;
-  if (parseInt(guess) === randomNumber) {
-    correctGuess = true;
-  }
-} while (!correctGuess)
-
-document.write("<p>You guessed the number!</p>");
-document.write("<p>It took you " + guessCount + " tries to guess the number: " + randomNumber + ".</p>");
+document.write(html);
