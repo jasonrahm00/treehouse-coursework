ebc4bab5dc2a4a6e9245d6671fff58be87c17aea
icesmoo00th <jasonrahm@gmail.com>
2016-04-18 15:45:04 -0600

    created simple number guessing game using a do/while look and prompt message asking the user to guess a number between 1 and 10

--- course8-js-loops-arrays-objs/script.js
+++ course8-js-loops-arrays-objs/script.js
@@ -1,16 +1,19 @@
-var upper = 10000;
-var randomNumber = getRandomNumber(upper);
+var randomNumber = getRandomNumber(10);
 var guess;
-var attempts = 0;
+var guessCount = 0;
+var correctGuess = false;
 
 function getRandomNumber(upper) {
   return Math.floor( Math.random() * upper ) + 1;
 }
 
-while ( guess !== randomNumber ) {
-  guess = getRandomNumber(upper);
-  attempts += 1;
-}
+do {
+  guess = prompt('I am thinking of a number between 1 and 10. What is it?');
+  guessCount += 1;
+  if (parseInt(guess) === randomNumber) {
+    correctGuess = true;
+  }
+} while (!correctGuess)
 
-document.write("<p>The random number was: " + randomNumber + "</p>");
-document.write("<p>It took the computer " + attempts + " attempts to get it right.</p>")
+document.write("<p>You guessed the number!</p>");
+document.write("<p>It took you " + guessCount + " tries to guess the number: " + randomNumber + ".</p>");
