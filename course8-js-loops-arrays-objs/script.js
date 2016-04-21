2a345e688298df48cbf2ca39ebd2602756840d4c
icesmoo00th <jasonrahm@gmail.com>
2016-04-21 16:30:53 -0600

    final commit for source8

--- course8-js-loops-arrays-objs/script.js
+++ course8-js-loops-arrays-objs/script.js
@@ -10,8 +10,6 @@ function print(message) {
   document.getElementById('results').innerHTML = message;
 }
 
-var message = '<p>Hello. My name is ' + person.name + '.</p>';
-message += '<p>I live in ' + person.city + '.</p>';
-message += '<p>I am ' + person.age + ' years old.</p>';
-message += '<p>I have the following skills: ' + person.skills.join(', ') + '.</p>'
-print(message);
+for (x in person) {
+  console.log(x, ': ', person[x]);
+}
