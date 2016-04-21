acb8e26331397c04de0b4a7b30d574b50ebe9881
icesmoo00th <jasonrahm@gmail.com>
2016-04-21 15:04:54 -0600

    Created simple quiz that auto generates ordered lists of correct and incorrect answers and displays them on the page

--- course8-js-loops-arrays-objs/script.js
+++ course8-js-loops-arrays-objs/script.js
@@ -1,22 +1,39 @@
-var playList = [
-  ['Free Bird', 'Lynyrd Skynyrd'],
-  ['Beautiful People', 'Marilyn Manson'],
-  ['Coma White', 'Marilyn Manson'],
-  ['Tainted Love', 'Marilyn Manson'],
-  ['Sweet Child O\' Mine', 'Guns N\' Roses'],
-  ['When The Levee Breaks', 'Led Zepplin']
-]
+var questions = [
+  ['How many states are in the U.S.?', 50],
+  ['How many continents are there?', 7],
+  ['How many legs does an insect have?', 6]
+];
+
+var question, answer, repsonse, html, correctAnswers = 0, correct = [], wrong = [];
 
 function print(message) {
-  document.write(message);
+  document.getElementById('results').innerHTML = message;
 }
 
-function printSongs( songs ) {
+function buildList(arr) {
   var listHTML = '<ol>';
-  for ( var i = 0; i < songs.length; i += 1) {
-    listHTML += '<li>' + songs[i][0] + ' by ' + songs[i][1] + '</li>';
+  for (var i = 0; i < arr.length; i += 1) {
+    listHTML += '<li>' + arr[i] + '</li>';
   }
   listHTML += '</ol>';
-  print(listHTML);
+  return listHTML;
 }
-printSongs(playList);
+
+for (var i = 0; i < questions.length; i += 1) {
+  question = questions[i][0];
+  answer = questions[i][1];
+  response = parseInt(prompt(question));
+  if (response === answer) {
+    correctAnswers += 1;
+    correct.push(question);
+  } else {
+    wrong.push(question);
+  }
+}
+
+html = "You got " + correctAnswers + " question(s) right.";
+html += "<h3>You go these questions correct:</h3>";
+html += buildList(correct);
+html += "<h3>You got these questions wrong:</h3>";
+html += buildList(wrong);
+print(html)
