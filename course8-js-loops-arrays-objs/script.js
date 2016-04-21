4e78f259f9cefd2363899b99d80c8ab54a492a40
icesmoo00th <jasonrahm@gmail.com>
2016-04-21 15:26:30 -0600

    created a person object with several properties and printed those to the screen

--- course8-js-loops-arrays-objs/script.js
+++ course8-js-loops-arrays-objs/script.js
@@ -1,39 +1,17 @@
-var questions = [
-  ['How many states are in the U.S.?', 50],
-  ['How many continents are there?', 7],
-  ['How many legs does an insect have?', 6]
-];
-
-var question, answer, repsonse, html, correctAnswers = 0, correct = [], wrong = [];
+var person = {
+  name: 'Jason',
+  city: 'Denver',
+  age: 34,
+  awesome: true,
+  skills: ['JavaScript', 'HTML', 'CSS', 'AngularJS', 'jQuery']
+};
 
 function print(message) {
   document.getElementById('results').innerHTML = message;
 }
 
-function buildList(arr) {
-  var listHTML = '<ol>';
-  for (var i = 0; i < arr.length; i += 1) {
-    listHTML += '<li>' + arr[i] + '</li>';
-  }
-  listHTML += '</ol>';
-  return listHTML;
-}
-
-for (var i = 0; i < questions.length; i += 1) {
-  question = questions[i][0];
-  answer = questions[i][1];
-  response = parseInt(prompt(question));
-  if (response === answer) {
-    correctAnswers += 1;
-    correct.push(question);
-  } else {
-    wrong.push(question);
-  }
-}
-
-html = "You got " + correctAnswers + " question(s) right.";
-html += "<h3>You go these questions correct:</h3>";
-html += buildList(correct);
-html += "<h3>You got these questions wrong:</h3>";
-html += buildList(wrong);
-print(html)
+var message = '<p>Hello. My name is ' + person.name + '.</p>';
+message += '<p>I live in ' + person.city + '.</p>';
+message += '<p>I am ' + person.age + ' years old.</p>';
+message += '<p>I have the following skills: ' + person.skills.join(', ') + '.</p>'
+print(message);
