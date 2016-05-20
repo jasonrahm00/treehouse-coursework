94e375a9af334140cb068b464cae28f22ec89d9c
icesmoo00th <jasonrahm@gmail.com>
2016-05-20 11:30:16 -0600

    jquery events created to hide/show password input alerts depending on password length and whether the two fields match

--- course10-jquery-basics/js/app.js
+++ course10-jquery-basics/js/app.js
@@ -1,2 +1,37 @@
 //Problem: Hints are shown even when form is valid
 //Solution: Hide and show hints at appropriate times
+
+var $password = $("#password");
+var $confirmPassword = $("#confirm_password");
+
+//Hide Hints
+$("form span").hide();
+
+function passwordEvent() {
+  //Find out if password is valid
+  if($password.val().length > 8) {
+    //Hide hint if valid
+    $password.next().hide();
+  } else {
+    //Show hint
+    $password.next().show();
+  }
+}
+
+function confirmPasswordEvent() {
+  //Find out if password and confirmation match
+  if($password.val() === $confirmPassword.val()) {
+    //Hide hint if matched
+    $confirmPassword.next().hide();
+   } else {
+    //Else show hint
+    $confirmPassword.next().show();
+   }
+}
+
+//When event happens on password input
+$password.focus(passwordEvent).keyup(passwordEvent).focus(confirmPasswordEvent).keyup(confirmPasswordEvent);
+  
+//When event happens on confirmation
+$confirmPassword.focus(confirmPasswordEvent).keyup(confirmPasswordEvent);
+  
