2ceb2dc83552b74cb04788392ffecd51b225395a
Jason Rahm <jasonrahm@gmail.com>
2016-08-18 16:01:00 -0600

    employee inoffice project refactored to use jQuery ajax

--- course15-ajax-basics/js/widget.js
+++ course15-ajax-basics/js/widget.js
@@ -1,43 +1,19 @@
-var xhr = new XMLHttpRequest();
-xhr.onreadystatechange = function() {
-  if(xhr.readyState === 4) {
-    if(xhr.status === 200) {
-      var employees = JSON.parse(xhr.responseText);
-      
-      //Call createEmployeeList function and pass in employees
-      createEmployeeList(employees);
-      
-    } else {
-      console.log(xhr.status + ' ' + xhr.statusText);
-    }
-  }
-};
-
-xhr.open('GET', 'data/employees.json');
-xhr.send();
-
-function createEmployeeList(x) {
+$(document).ready(function () {
   
-  var list = document.getElementById('employeeList');
+  var url = "../data/employees.json";
   
-  var ul = document.createElement('ul');
-  ul.className = 'bulleted';
-  list.appendChild(ul);
+  $.getJSON(url, function(response) {
+    var statusHTML = '<ul class="bulleted">';
+    $.each(response, function(index, employee) {
+      if(employee.inoffice === true) {
+        statusHTML += '<li class="in">';
+      } else {
+        statusHTML += '<li class="out">';
+      }
+      statusHTML += employee.name + '</li>';
+    }); //end each loop
+    statusHTML += '</ul>';
+    $('#employeeList').html(statusHTML);
+  }); //end getJSON
   
-  //Iterate over each employee in the JSON object array
-    //Create li for each person and set innerText to their name
-    //Add class in/out depending on the inoffice value
-    //Append list item to the unordered list to so it displays on the page
-  for(var i = 0; i < x.length; i++) {
-    var y = x[i];
-    var li = document.createElement('li');
-    if(y.inoffice) {
-      li.className = 'in';
-    } else {
-      li.className = 'out';
-    }
-    li.innerText = y.name;
-    ul.appendChild(li);
-  }
-  
-}
+}); // end ready
