7f84228a2e5699f1dd94a6522eaee96dc28d8e9a
Jason Rahm <jasonrahm@gmail.com>
2016-08-21 12:36:02 -0600

    dice object tranformed into constructor

--- course16-oojs/dice.js
+++ course16-oojs/dice.js
@@ -1,7 +1,9 @@
-var dice = {
-  sides: 6,
-  roll: function () {
+function Dice(sides) {
+  this.sides = 6;
+  this.roll = function () {
     var randomNumber = Math.floor(Math.random() * this.sides) + 1;
     return randomNumber;
   }
 }
+
+var dice = new Dice(6);
