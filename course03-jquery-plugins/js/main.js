a6d8d26d06d22546a0d602e98c6fe97b9d04e238
Jason Rahm <jasonrahm@gmail.com>
2016-09-02 15:22:02 -0600

    object literal properties added to main plugin to adjust transition timing and animation

--- course03-jquery-plugins/js/main.js
+++ course03-jquery-plugins/js/main.js
@@ -1,1 +1,7 @@
-$(".animsition").animsition();
+$(".animsition").animsition({
+  inClass: "fade-in-right-lg",
+  outClass: "fade-out-right-lg",
+  linkElement: "header a",
+  inDuration: 1500,
+  outDuration: 1000
+});
