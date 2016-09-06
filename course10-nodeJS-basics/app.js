c7c3b13929d827723132a5408d215b04fe4f4bab
Jason Rahm <jasonrahm@gmail.com>
2016-09-06 17:06:43 -0600

    initial commit and setup for node.js basics course project on reading and parsing teamtreehouse user profile information

---
+++ course10-nodeJS-basics/app.js
@@ -0,0 +1,7 @@
+//Problem: We need a simple way to look at a user's badge acount and JavaScript points
+//Solution: Use Node.js to connect with Treehouse's API to get profile information to print out
+
+//Connect to the API URL (http://teamtreehouse.com.username.json)
+//Read data
+//Parse data
+//Print data
