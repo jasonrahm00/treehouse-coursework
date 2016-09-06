282670f5dbadfc83d86303654d9bd23495de8372
Jason Rahm <jasonrahm@gmail.com>
2016-09-06 17:50:27 -0600

    solution for node program to log total js points on my teamtreehouse profile

--- course10-nodeJS-basics/app.js
+++ course10-nodeJS-basics/app.js
@@ -1,7 +1,29 @@
 //Problem: We need a simple way to look at a user's badge acount and JavaScript points
 //Solution: Use Node.js to connect with Treehouse's API to get profile information to print out
+const https = require("https");
+var username = "jasonrahm";
 
-//Connect to the API URL (http://teamtreehouse.com.username.json)
-//Read data
-//Parse data
-//Print data
+function printMessage(username, badgeCount, points) {
+  var message = username + " has " + badgeCount + " total badge(s) and " + points + " points in JavaScript.";
+  console.log(message);
+ }
+
+//Connect to the API URL (https://teamtreehouse.com/username.json)
+var request = https.get('https://teamtreehouse.com/' + username + '.json', function(response) {
+  var body = "";
+  
+  //Read data
+  response.on('data', function(chunk) {
+    body += chunk;
+  });
+  response.on('end', function() {
+    //Parse data as JSON
+    var profile = JSON.parse(body);
+    //Print data
+    printMessage(username, profile.badges.length, profile.points.JavaScript);
+  });
+});
+
+request.on("error", function(e) {
+  console.error(e);
+});
