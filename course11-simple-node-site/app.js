0d6f92b6063a2a7581db702b551e0d952662585b
Jason Rahm <jasonrahm@gmail.com>
2016-09-07 11:33:59 -0600

    node server created and routing added to handle a homepage and one to display user information based on url

---
+++ course11-simple-node-site/app.js
@@ -0,0 +1,21 @@
+var router = require("./router.js");
+//Problem: Need a simple way to look at a users badge count and js points from a web browser
+//Solution: Use Node.js to perform the profile look ups and serve our templates via HTTP
+
+//Create a web server
+const http = require('http');
+const hostname = '127.0.0.1';
+const port = 8080;
+
+http.createServer((request, response) => {
+  router.home(request, response);
+  router.user(request, response);
+}).listen(port);
+
+console.log(`Server running at http://${hostname}:${port}/`);
+
+
+
+//Function that handles the reading of template files and merge in values
+  //read from file and get a string
+    //merge values into string
