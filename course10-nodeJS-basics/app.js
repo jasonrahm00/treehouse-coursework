6f76a16730db0c06dca692faa4e5e1a27bd8d9f0
Jason Rahm <jasonrahm@gmail.com>
2016-09-06 18:09:08 -0600

    more detailed error handling added

--- course10-nodeJS-basics/app.js
+++ course10-nodeJS-basics/app.js
@@ -1,12 +1,19 @@
 //Problem: We need a simple way to look at a user's badge acount and JavaScript points
 //Solution: Use Node.js to connect with Treehouse's API to get profile information to print out
 const https = require("https");
+const http = require("http");
 var username = "jasonrahm";
 
+//Pritn out message
 function printMessage(username, badgeCount, points) {
   var message = username + " has " + badgeCount + " total badge(s) and " + points + " points in JavaScript.";
   console.log(message);
  }
+
+//Print out error messages
+function printError(error) {
+  console.error(error.message);
+}
 
 //Connect to the API URL (https://teamtreehouse.com/username.json)
 var request = https.get('https://teamtreehouse.com/' + username + '.json', function(response) {
@@ -17,13 +24,22 @@ var request = https.get('https://teamtreehouse.com/' + username + '.json', funct
     body += chunk;
   });
   response.on('end', function() {
-    //Parse data as JSON
-    var profile = JSON.parse(body);
-    //Print data
-    printMessage(username, profile.badges.length, profile.points.JavaScript);
+    if(response.statusCode === 200) {
+      try {
+        //Parse data as JSON
+        var profile = JSON.parse(body);
+        //Print data
+        printMessage(username, profile.badges.length, profile.points.JavaScript);
+      } catch(error) {
+        //Pare error
+        printError(error);
+      }
+    } else {
+      //Status Code Error
+      printError({message: "There was an error gettign the profile for " + username + ". (" + http.STATUS_CODES[response.statusCode] + ")"});
+    }
   });
 });
 
-request.on("error", function(e) {
-  console.error(e);
-});
+//Connection error
+request.on("error", printError);
