647581ddc6e520ddb2890ace853ca0d1367d7929
Jason Rahm <jasonrahm@gmail.com>
2016-09-07 13:27:50 -0600

    POST and GET handling added to allow search field functionality

--- course11-simple-node-site/router.js
+++ course11-simple-node-site/router.js
@@ -1,21 +1,33 @@
 var Profile = require("./profile.js");
 var render = require("./render.js");
-
+var queryString = require("querystring");
+var commonHeader = {'Content-Type': 'text/html'};
+    
 //Handle HTTPS route GET / and POST  / i.e. Home
 function home(request, response) {
   //if url == "/" && GET
   if (request.url === "/") {
-    //show search
-    response.writeHead(200, {'Content-Type': 'text/plain'});
-    render.view('header', {}, response);
-    render.view('search', {}, response);
-    render.view('footer', {}, response);
-    //response.write("Search\n");
-    //response.end('Footer\n');
-    response.end();
+    if(request.method.toLowerCase() === "get") {
+      //show search
+      response.writeHead(200, commonHeader);
+      render.view('header', {}, response);
+      render.view('search', {}, response);
+      render.view('footer', {}, response);
+      //response.write("Search\n");
+      //response.end('Footer\n');
+      response.end();
+    } else {
+      //if url == "/" && POST
+        //get the POST data from body
+        request.on("data", function(postBody) {
+          //extract username
+          var query = queryString.parse(postBody.toString());
+          //redirect to /:username
+          response.writeHead(303, {"location": "/" + query.username});
+          response.end();
+        }); 
+    }
   }    
-  //if url == "/" && POST
-    //redirect to /:username
 }
   
 //Handle HTTPS route for GET/:username i.e. /chalkers
@@ -23,7 +35,7 @@ function user(request, response) {
   //if url == "/..."
   var username = request.url.replace("/", "");
   if(username.length > 0) {
-    response.writeHead(200, {'Content-Type': 'text/plain'});
+    response.writeHead(200, commonHeader);
     
     render.view('header', {}, response);
     
