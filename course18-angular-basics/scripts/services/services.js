865af11e109492cc23e26360c450aec0516cc662
Jason Rahm <jasonrahm@gmail.com>
2016-10-07 22:24:35 -0400

    final solution for todo list project

---
+++ course18-angular-basics/scripts/services/services.js
@@ -0,0 +1,17 @@
+'use strict';
+angular.module("todoListApp")
+.service('dataService', function($http){
+  
+  this.getTodos = function(callback){
+    $http.get('mock/todos.json').then(callback)
+  };
+  
+  this.deleteTodo = function(todo){
+    console.log("The " + todo.name + " todo has been deleted.");
+  };
+  
+  this.saveTodos = function(todos) {
+    console.log(todos.length + " todos have been saved.");
+  };
+  
+});
